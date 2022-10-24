ad98f158ab6c78808c1fb2b22824aeb574f6a7b1
Gabriel Salinas Szada <g.szada@gmail.com>
2022-10-24 13:09:35 +0200

    built a layout for the nav buttons

--- src/components/buttons/FavoritesButton.vue
+++ src/components/buttons/FavoritesButton.vue
@@ -1,50 +1,23 @@
 <script setup>
-  import {useRoute} from 'vue-router'
+    import {useRoute} from 'vue-router'
     import {computed} from 'vue'
+    import NavButtonLayout from '@/layouts/NavButtonLayout.vue'
 
-    const route = useRoute()
-    const currentPath = computed(() => route.path)
+    const currentPath = computed(() => useRoute().path)
 
     function pathIsFavorites(){
-        if(currentPath.value === '/favorites'){
-            return true
-        }
-        else{
-            return false
-        }
+        return currentPath.value === '/favorites' ? true : false
     }
 </script>
 
 <template>
     <router-link to="/favorites">
-        <div v-if="pathIsFavorites()" class="profile-link-wrapper">
-            <span class="material-symbols-outlined fill-icon">
-                favorite
-            </span>
-        </div>
-        <div v-else class="profile-link-wrapper">
-            <span class="material-symbols-outlined">
-                favorite
-            </span>
-        </div>
+        <NavButtonLayout :show="pathIsFavorites()" iconName="favorite" />
     </router-link>
 </template>
 
 <style scoped>
 a{
     text-decoration: none;
-}
-.profile-link-wrapper{
-    display:grid;
-    place-items: center;
-    aspect-ratio: 1;
-    height:60px;
-    border-left:1px solid #e0e0e0;
-    border-bottom:1px solid #e0e0e0;
-    cursor:pointer;
-    color:var(--dark-navy);
-}
-.profile-link-wrapper:hover{
-    background:#fafafe;
 }
 </style>
---
+++ src/layouts/NavButtonLayout.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import {defineProps} from 'vue'
+
+    const props = defineProps({show:Boolean, iconName:String})
+</script>
+
+<template>
+    <slot name="button-content">
+        <div v-if="props.show" class="profile-link-wrapper">
+            <span class="material-symbols-outlined fill-icon">
+                {{props.iconName}}
+            </span>
+        </div>
+        <div v-else class="profile-link-wrapper">
+            <span class="material-symbols-outlined">
+                {{props.iconName}}
+            </span>
+        </div>
+    </slot>
+</template>
+
+<style scoped>
+.profile-link-wrapper{
+    display:grid;
+    place-items: center;
+    aspect-ratio: 1;
+    height:60px;
+    border-left:1px solid #e0e0e0;
+    border-bottom:1px solid #e0e0e0;
+    cursor:pointer;
+    color:var(--dark-navy);
+}
+.profile-link-wrapper:hover{
+    background:#fafafe;
+}
+</style>
