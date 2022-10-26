e847308df00ead48e4c24fc25cb69cc96892ebdf
Gabriel Salinas Szada <g.szada@gmail.com>
2022-10-26 12:54:24 +0200

    the favorites option in the menu is now only shown when the user is logged in

--- src/components/menuOptions/MenuOptions.vue
+++ src/components/menuOptions/MenuOptions.vue
@@ -4,18 +4,21 @@
     import AlbumButton from '@/components/menuOptions/children/AlbumButton.vue'
     import FavoritesOption from './children/FavoritesOption.vue'
 
-    const store = useStore() 
-    const maxAlbums = computed(() => store.state.albums.maxToShow)
-    const allAlbums = computed(() => store.state.albums.data)
-    const albumsToShow = computed(() => allAlbums.value.slice(0, maxAlbums.value))
+    const store = useStore()
+
+    const 
+        userIsLogged = computed(() => store.getters['auth/userIsLogged']),
+        maxAlbums = computed(() => store.state.albums.maxToShow),
+        allAlbums = computed(() => store.state.albums.data),
+        albumsToShow = computed(() => allAlbums.value.slice(0, maxAlbums.value));
 </script>
 
 <template>
-    <FavoritesOption />
-    <div v-for="album in albumsToShow" :key="album.id">
-        <AlbumButton
-            :albumId="album.id"
-            :albumTitle="album.title" 
-        />
-    </div>
+    <FavoritesOption v-if="userIsLogged" />
+    <AlbumButton
+        v-for="album in albumsToShow" 
+        :key="album.id"
+        :albumId="album.id"
+        :albumTitle="album.title" 
+    />
 </template>
